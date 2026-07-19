58af21ada6596cb54c03bbb9328f0d91205ea4cc
jatipathak816-blip <jatinpathak816@gmail.com>
2026-07-19 12:32:52 +0530

    Add PDF file

--- module1/sol2.cpp
+++ module1/sol2.cpp
@@ -66,48 +66,49 @@ int main()
 
 
 
-{
-    int n;
-    cin >> n;
+// {
+//     int n;
+//     cout << "Enter the number of rows for pyramid number pattern: ";
+//     cin >> n;
 
-    // Upper inverted pyramid
-    for (int i = n; i >= 1; i--) {
+//     // Upper inverted pyramid
+//     for (int i = n; i >= 1; i--) {
 
-        // Spaces
-        for (int j = 0; j < n - i; j++)
-            cout << " ";
+//         // Spaces
+//         for (int j = 0; j < n - i; j++)
+//             cout << " ";
 
-        // Increasing numbers
-        for (int j = 1; j <= i; j++)
-            cout << j;
+//         // Increasing numbers
+//         for (int j = 1; j <= i; j++)
+//             cout << j;
 
-        // Decreasing numbers
-        for (int j = i - 1; j >= 1; j--)
-            cout << j;
+//         // Decreasing numbers
+//         for (int j = i - 1; j >= 1; j--)
+//             cout << j;
 
-        cout << endl;
-    }
+//         cout << endl;
+//     }
 
-    // Lower pyramid
-    for (int i = 2; i <= n; i++) {
+//     // Lower pyramid
+//     for (int i = 2; i <= n; i++) {
 
-        // Spaces
-        for (int j = 0; j < n - i; j++)
-            cout << " ";
+//         // Spaces
+//         for (int j = 0; j < n - i; j++)
+//             cout << " ";                         // sandwicth pattern
 
-        // Increasing numbers
-        for (int j = 1; j <= i; j++)
-            cout << j;
+//         // Increasing numbers
+//         for (int j = 1; j <= i; j++)
+//             cout << j;
 
-        // Decreasing numbers
-        for (int j = i - 1; j >= 1; j--)
-            cout << j;
+//         // Decreasing numbers
+//         for (int j = i - 1; j >= 1; j--)
+//             cout << j;
 
-        cout << endl;
-    }
+//         cout << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 
